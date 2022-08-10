e03f64a6c3d8ba620c3a35c5d1509ef22d2e583a
Theo Kempf elbaz <tkempf-e@k0r3p6.42mulhouse.fr>
2022-08-10 11:24:07 +0200

    arguments_checker

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tkempf-e <tkempf-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 10:02:48 by tkempf-e          #+#    #+#             */
-/*   Updated: 2022/08/10 11:15:43 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2022/08/10 11:23:57 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,10 +53,19 @@ int	number_checker(char *argv[])
 	Checking if arguments are numbers.
 	Converting them from char* to int.
 */
-int	argchecker(int argc, char *argv[], t_data *data)
+int	arguments_checker(int argc, char *argv[], t_data *data)
 {
-	if (number_checker(argv) == -1)
+	if (number_checker(argv) == -1 || argc < 5 || argc > 6)
 		return (-1);
+	data->number_of_philosopher = ft_atoi(argv[1]);
+	data->time_to_die = ft_atoi(argv[2]);
+	data->time_to_eat = ft_atoi(argv[3]);
+	data->time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		data->times_each_philosopher_must_eat = ft_atoi(argv[5]);
+	else if (argc == 5)
+		data->times_each_philosopher_must_eat = 0;
+	return (0);
 }
 
 int	ft_error(void)
@@ -70,6 +79,6 @@ int	main(int argc, char *argv[])
 {
 	t_data	data;
 
-	if (arg_checker(argc, argv, &data) == -1)
+	if (arguments_checker(argc, argv, &data) == -1)
 		return (ft_error);
 }
